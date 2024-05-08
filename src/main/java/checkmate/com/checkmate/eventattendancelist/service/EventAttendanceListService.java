900bbf1e06bb1d9ab4b2a82d4e6a6ad89a2c7d5e
영서 <dudrhy12@sookmyung.ac.kr>
2024-05-08 16:01:05 +0900

    Feat: Add Sign API service code

--- src/main/java/checkmate/com/checkmate/eventattendancelist/service/EventAttendanceListService.java
+++ src/main/java/checkmate/com/checkmate/eventattendancelist/service/EventAttendanceListService.java
@@ -7,11 +7,14 @@ import checkmate.com.checkmate.eventattendancelist.domain.EventAttendanceList;
 import checkmate.com.checkmate.eventattendancelist.domain.repository.EventAttendanceListRepository;
 import checkmate.com.checkmate.eventattendancelist.dto.StudentInfoResponseDto;
 import checkmate.com.checkmate.eventschedule.domain.repository.EventScheduleRepository;
+import checkmate.com.checkmate.global.config.S3Uploader;
+import checkmate.com.checkmate.user.domain.User;
 import checkmate.com.checkmate.user.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
@@ -23,6 +26,8 @@ public class EventAttendanceListService {
     private final EventScheduleRepository eventScheduleRepository;
     @Autowired
     private final EventAttendanceListRepository eventAttendanceListRepository;
+    @Autowired
+    private final S3Uploader s3Uploader;
 
     @Transactional
     public StudentInfoResponseDto getStudentInfo(Long userId, Long eventId, int studentId, String eventDate){
@@ -32,6 +37,11 @@ public class EventAttendanceListService {
         return StudentInfoResponseDto.of(studentInfoFromEventAttendance, eventTitle);
     }
 
-
+    @Transactional
+    public void postSign(Long userId, Long eventId, Long studentInfoId, MultipartFile signImage){
+        EventAttendanceList eventAttendanceList = eventAttendanceListRepository.findByEventAttendanceListId(studentInfoId);
+        String imageUrl = s3Uploader.saveFile(signImage, String.valueOf(userId), "event"+String.valueOf(eventId));
+        eventAttendanceList.setSign(imageUrl);
+    }
 
 }
