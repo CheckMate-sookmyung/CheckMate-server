bef7dfd865a04717d52995bf145d1f099618156e
영서 <dudrhy12@sookmyung.ac.kr>
2024-06-28 23:45:08 +0900

    Fix: Modify ErrorCode

--- src/main/java/checkmate/com/checkmate/global/codes/ErrorCode.java
+++ src/main/java/checkmate/com/checkmate/global/codes/ErrorCode.java
@@ -38,7 +38,7 @@ public enum ErrorCode {
 
     //출석체크
     STUDENT_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 학생 정보입니다."),
-    STUDENT_ALREADY_CHECK(HttpStatus.ALREADY_REPORTED, "이미 출석체크한 학생입니다.");
+    STUDENT_ALREADY_CHECK(HttpStatus.CONFLICT, "이미 출석체크한 학생입니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
