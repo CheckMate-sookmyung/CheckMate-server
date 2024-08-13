be0ce959a22f53e08222290e33a07a4b5ef7852b
영서 <dudrhy12@sookmyung.ac.kr>
2024-08-14 00:06:07 +0900

    Fix: Modify User Entity

--- src/main/java/checkmate/com/checkmate/user/domain/User.java
+++ src/main/java/checkmate/com/checkmate/user/domain/User.java
@@ -3,6 +3,7 @@ package checkmate.com.checkmate.user.domain;
 import checkmate.com.checkmate.event.domain.Event;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.DynamicInsert;
@@ -25,8 +26,38 @@ public class User {
     private String userName;
 
     @Column(nullable = false)
-    private String userMail;
+    private String userEmail;
+
+    @Column(nullable = false)
+    private String socialId;
+
+    @Column(nullable = false)
+    private String refreshToken;
+
+    @Column(nullable = false)
+    private String socialRefreshToken;
 
     @OneToMany(mappedBy = "user")
     private List<Event> events = new ArrayList<>();
+
+    @Builder
+    public User(final String userName,
+                final String userEmail,
+                final String socialId,
+                final String refreshToken,
+                final String socialRefreshToken){
+        this.userName = userName;
+        this.userEmail = userEmail;
+        this.socialId = socialId;
+        this.refreshToken = refreshToken;
+        this.socialRefreshToken = socialRefreshToken;
+    }
+
+    public void updateUser(String userName){
+        this.userName = userName;
+    }
+
+    public void updateRefreshToken(final String refreshToken){
+        this.refreshToken = refreshToken;
+    }
 }
