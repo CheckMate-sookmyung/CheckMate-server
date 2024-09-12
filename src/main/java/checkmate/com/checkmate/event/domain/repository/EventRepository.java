3e75e86ceb19b6fddc7106b4fade83b14a11dbed
영서 <dudrhy12@sookmyung.ac.kr>
2024-09-12 13:54:34 +0900

    Feat: Event Repository Method 추가

--- src/main/java/checkmate/com/checkmate/event/domain/repository/EventRepository.java
+++ src/main/java/checkmate/com/checkmate/event/domain/repository/EventRepository.java
@@ -13,4 +13,15 @@ public interface EventRepository extends JpaRepository<Event, Long> {
 
     @Query("SELECT e FROM Event e WHERE e.member.id = :memberId")
     List<Event> findByMemberId(@Param("memberId") Long memberId);
+
+    @Query("SELECT COUNT(ea) " +
+            "FROM EventAttendance ea " +
+            "JOIN ea.eventSchedule es " +
+            "JOIN es.event e " +
+            "WHERE e.eventId = :eventId " +
+            "AND ea.attendance = true")
+    int countAttendanceForEvent(@Param("eventId") Long eventId);
+
+    @Query("SELECT e.completionTime FROM Event e WHERE e.eventId = :eventId")
+    Integer findCompletionTimeByEventId(@Param("eventId") Long eventId);
 }
