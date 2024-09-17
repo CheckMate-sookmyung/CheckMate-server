c289885309b7088e43f73bf5a18a548c5cd52cb6
영서 <dudrhy12@sookmyung.ac.kr>
2024-09-17 22:17:20 +0900

    Fix: 메일 줄바꿈 문자 수정

--- src/main/java/checkmate/com/checkmate/mail/service/MailService.java
+++ src/main/java/checkmate/com/checkmate/mail/service/MailService.java
@@ -77,15 +77,15 @@ public class MailService {
         List<EventSchedule> eventSchedules = eventScheduleRepository.findEventScheduleListByEventId(event.getEventId());
         String title = "[체크메이트]" + event.getEventTitle() + "리마인드 메일";
         StringBuilder content = new StringBuilder();
-        content.append("안녕하세요.\n");
-        content.append("내일").append(event.getEventTitle()).append("이(가) 진행됩니다! 참석을 잊지 마시고, 일정을 꼭 확인해주세요.\n\n");
-        content.append("행사명: ").append(event.getEventTitle()).append("\n");
-        content.append("내용: ").append(event.getEventDetail()).append("\n");
+        content.append("안녕하세요.<br>");
+        content.append("내일").append(event.getEventTitle()).append("이(가) 진행됩니다! 참석을 잊지 마시고, 일정을 꼭 확인해주세요.<br><br>");
+        content.append("행사명: ").append(event.getEventTitle()).append("<br>");
+        content.append("내용: ").append(event.getEventDetail()).append("<br>");
         content.append("일시: ");
         for (EventSchedule eventSchedule : eventSchedules) {
             content.append(eventSchedule.getEventDate()).append(" - ").append(eventSchedule.getEventStartTime()).append("~").append(eventSchedule.getEventEndTime()).append(", ");
         }
-        content.append("\n감사합니다.");
+        content.append("<br>감사합니다.");
         Mail mail = Mail.builder()
                 .mailType(REMIND)
                 .mailTitle(title)
@@ -100,7 +100,7 @@ public class MailService {
     public void createSurveyMail(Event event) {
         String title = "[체크메이트]" + event.getEventTitle() + "설문조사 안내 메일";
         StringBuilder content = new StringBuilder();
-        content.append("안녕하세요.\n").append(event.getEventTitle()).append("만족도 조사 안내드립니다.\n감사합니다.");
+        content.append("안녕하세요.<br>").append(event.getEventTitle()).append("만족도 조사 안내드립니다.<br>감사합니다.");
         Mail mail = Mail.builder()
                 .mailType(MailType.SURVEY)
                 .mailTitle(title)
