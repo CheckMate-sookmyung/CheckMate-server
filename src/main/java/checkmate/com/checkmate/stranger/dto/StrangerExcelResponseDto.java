ca920b86aa7c10adc1c362ad3cffda1b054cbcc3
영서 <dudrhy12@sookmyung.ac.kr>
2024-09-23 11:42:58 +0900

    Fix: DTO 설명 추가

--- src/main/java/checkmate/com/checkmate/stranger/dto/StrangerExcelResponseDto.java
+++ src/main/java/checkmate/com/checkmate/stranger/dto/StrangerExcelResponseDto.java
@@ -1,6 +1,7 @@
 package checkmate.com.checkmate.stranger.dto;
 
 import checkmate.com.checkmate.student.dto.StudentExcelResponseDto;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -8,10 +9,19 @@ import static lombok.AccessLevel.PRIVATE;
 
 @Getter
 @RequiredArgsConstructor(access=PRIVATE)
+@Schema(description = "Stranger Excel Response")
 public class StrangerExcelResponseDto {
+
+    @Schema(description = "외부인 이름", example = "홍길동")
     private final String strangerName;
+
+    @Schema(description = "외부인 전화번호", example = "010-1234-5678")
     private final String strangerPhoneNumber;
+
+    @Schema(description = "외부인 메일주소", example = "abcd@gmail.com")
     private final String strangerEmail;
+
+    @Schema(description = "외부인 소속", example = "LG")
     private final String strangerAffiliation;
 
     public static StrangerExcelResponseDto of(String strangerName, String strangerPhoneNumber, String strangerEmail, String strangerAffiliation){
--- src/main/java/checkmate/com/checkmate/student/dto/StudentExcelResponseDto.java
+++ src/main/java/checkmate/com/checkmate/student/dto/StudentExcelResponseDto.java
@@ -1,5 +1,6 @@
 package checkmate.com.checkmate.student.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -7,11 +8,22 @@ import static lombok.AccessLevel.PRIVATE;
 
 @Getter
 @RequiredArgsConstructor(access=PRIVATE)
+@Schema(description = "Student Excel Response")
 public class StudentExcelResponseDto {
+
+    @Schema(description = "학생 이름", example = "김눈송")
     private final String studentName;
+
+    @Schema(description = "학생 학번", example = "1234567")
     private final int studentNumber;
+
+    @Schema(description = "학생 학과", example = "컴퓨터과학전공")
     private final String studentMajor;
+
+    @Schema(description = "학생 전화번호", example = "010-5336-5708")
     private final String studentPhoneNumber;
+
+    @Schema(description = "학생 메일주소", example = "checkmatesmwu@gmail.com")
     private final String studentEmail;
 
     public static StudentExcelResponseDto of(String studentName, int studentNumber, String studentMajor, String studentPhoneNumber, String studentEmail){
